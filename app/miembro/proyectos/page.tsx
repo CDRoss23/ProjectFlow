eff23cc3aeec48857b6db363de4e7e19b3cf3029
CDRoss23 <rossmancd23@gmail.com>
2025-03-24 22:24:04 -0600

    ahora si funciona

--- app/miembro/proyectos/page.tsx
+++ app/miembro/proyectos/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import Sliderbar from '../componentes/sliderbar';
+import Sliderbar from '../../componentes/sliderbar';
 import {
     DndContext,
     closestCorners,
